961ccc9b67502e876e99ada0ecc911586217db90
Pramila Shanmugam <pramila.sl@gmail.com>
2024-06-04 08:43:13 +0000

    setting.py updated

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['8000-pramilashanm-djangoblog-p10mun4738d.ws-eu114.gitpod.io',
+ALLOWED_HOSTS = ['8000-pramilashanm-djangoblog-8l8er290qr5.ws.codeinstitute-ide.net',
                  '.herokuapp.com']
 
 
@@ -90,6 +90,11 @@ DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeanyapp.com",
+    "https://*.herokuapp.com",
+    "https://*.8000-pramilashanm-djangoblog-8l8er290qr5.ws.codeinstitute-ide.net"
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
